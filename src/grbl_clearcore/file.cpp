1f7e7756aa9689041258516244e1794b004c98da
Craig Hollabaugh <holla2040@gmail.com>
2021-06-15 15:17:47 -0600

    sd settings read and write now functioning

--- src/grbl_clearcore/file.cpp
+++ src/grbl_clearcore/file.cpp
@@ -9,6 +9,8 @@ extern "C" {
 
 File settingsFile;
 
+//#define DEBUGFILE1
+
 void sdInit(void) {
     if (!SD.begin()) {
         console.println("SD begin failed");
@@ -19,44 +21,60 @@ void sdInit(void) {
 }
 
 bool sdSettingsRead(uint8_t *dest) {
-  return false;
-    console.print("sdRead ");
+#ifdef DEBUGFILE
+      console.print("sdRead ");
+#endif
+
     settingsFile = SD.open("settings.bin");
     if (settingsFile) {
       settingsFile.read(dest,GRBL_NVS_SIZE);
       settingsFile.close();
+#ifdef DEBUGFILE
       console.println("ok");
+#endif
       return true;
     } 
+#ifdef DEBUGFILE
     console.println("fail");
+#endif
     return false;
 }
 
 bool sdSettingsWrite(uint8_t *source) {
+#ifdef DEBUGFILE
     console.print("sdWrite ");
+#endif
     SD.remove("settings.bin");
     settingsFile = SD.open("settings.bin",FILE_WRITE);
     if (settingsFile) {
       settingsFile.write(source,GRBL_NVS_SIZE);
       settingsFile.close();
+#ifdef DEBUGFILE
       console.println("ok");
+#endif
       return true;
     } 
+#ifdef DEBUGFILE
     console.println("fail");
+#endif
     return false;
 }
 
 uint8_t sdSettingsGetByte(uint32_t addr) {
+#ifdef DEBUGFILE
   console.print("sdGetByte ");
   console.println(addr,HEX);
+#endif
   return SETTINGS_VERSION;
 }
 
 void sdSettingsPutByte(uint32_t addr, uint8_t new_value) {
+#ifdef DEBUGFILE
   console.print("sdPutByte ");
   console.print(addr,HEX);
   console.print(" ");
   console.println(new_value,HEX);
+#endif
 }
 
 
